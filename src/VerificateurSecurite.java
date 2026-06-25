1744275c28d0122e95909e353c58fdb7e66a565f
VICKY-2505 <victoireamoi488@gmail.com>
2026-06-25 11:34:49 +0000

    rename password checker en password

--- src/VerificateurSecurite.java
+++ src/VerificateurSecurite.java
@@ -11,7 +11,7 @@ public class VerificateurSecurite {
                     "docker",
                     "run",
                     "--rm",
-                    "password-checker",
+                    "password",
                     mdp
             );
 
